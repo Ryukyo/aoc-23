5c80f57b311995717f2e3615af3d0613cb0ce892
Ryukyo <florian.fejer@hotmail.de>
2023-12-02 11:38:50 +0100

    day2 - part2

--- day2.py
+++ day2.py
@@ -1,4 +1,5 @@
 import regex as re
+import math
 
 sum_possible = 0
 bag_limit = {
@@ -7,10 +8,14 @@ bag_limit = {
     "blue": 14
 }
 
+round_max = {}
+i = 1
+
 with open('day2.txt', 'r') as f:
     for idx, line in enumerate(f.readlines()):
         valid = True
-        line.strip()
+        round_max[i] = {}
+
         game, results = line.split(": ")
         game_id = game.split(" ")[1]
         round_data = re.findall(r"\d+\s(?:red|blue|green)", results)
@@ -23,6 +28,21 @@ with open('day2.txt', 'r') as f:
             if value > bag_limit[color]:
                 valid = False
 
+            # get the maximum of each 
+            if color not in round_max[i]:
+                round_max[i][color] = value
+            if value > round_max[i][color]:
+                round_max[i][color] = value
+        
+        i += 1
         if valid:
             sum_possible += int(game_id)
+
     print(sum_possible)
+
+    t_num = 0
+
+    for nums in round_max.values():
+        t_num += math.prod(nums.values())
+
+    print(t_num)

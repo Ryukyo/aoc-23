deff1f55b18697eebf596f812f441b6c2ae5e1f0
Ryukyo <florian.fejer@hotmail.de>
2023-12-05 21:16:38 +0100

    day5 - part2

--- day5.py
+++ day5.py
@@ -13,13 +13,42 @@ for mp in entries[1:]:
         m.append([start, start + delta - 1, end - start])
     maps.append(sorted(m))
 
-def lookup(s, map):
-    for start, end, delta in map:
+def lookup(s, m):
+    for start, end, delta in m:
         if   s > end: continue
         elif s < start: return s
         else: return s + delta
     return s
 
+def lookup2(s, t, m):
+    rs = []
+
+    for start, end, delta in m:
+        # start.........end  [s-t] or: [s-t] start.........end
+        if s > end or t < start:
+            continue
+        if s < start: # [s--start-----?t].end  ?t]
+            rs += [(s, start - 1), (start + delta, min(end, t) + delta)]
+        else: # start.[s--?t].end  ?t]
+            rs += [(s + delta, min(end, t) + delta)]
+
+        # start.[s--t]..end
+        if end > t: return rs   
+        s = end
+    if not rs: rs = [(s, t)]
+    return rs
+
+def process2(p):
+    r = [(p[0], p[0] + p[1])]
+    for m in maps:
+        rs = []
+        for s, t in r:
+            rs += lookup2(s, t, m)
+        r = rs
+    return min(rs)[0]
+
 locations  = [reduce(lookup, maps, s) for s in seeds]
+locations2 = [process2((seeds[i:i + 2])) for i in range(0, len(seeds), 2)]
 
 print(f"Part1: {min(locations)}")
+print(f"Part2: {min(locations2)}")

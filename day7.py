26c422c92f5512a28e0a00366403fadbf8c5e571
Ryukyo <florian.fejer@hotmail.de>
2023-12-07 21:57:03 +0100

    day7 - part1

---
+++ day7.py
@@ -0,0 +1,39 @@
+from collections import Counter
+
+hands_bets = open("day7.txt").read().rstrip()
+
+def hand_type(hand):
+    c = Counter(hand)
+    counts = sorted(c.values())
+
+    match counts:
+        case *_, 5:
+            return 7
+        case *_, 4:
+            return 6
+        case *_, 2, 3:
+            return 5
+        case *_, 3:
+            return 4
+        case *_, 2, 2:
+            return 3
+        case *_, 2:
+            return 2
+    return 1
+
+
+def solve(data):
+    ws = [l.split() for l in data.split("\n")]
+    return sum(
+        rank * bid
+        for rank, (*_, bid) in enumerate(
+            sorted(
+                (hand_type(hand), *map("*23456789TJQKA".index, hand), int(bid))
+                for hand, bid in ws
+            ),
+            1,
+        )
+    )
+
+# Part 1
+print(solve(hands_bets))

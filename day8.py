7d92bce0c746ce9680c39613406a31b73d91a86a
Ryukyo <florian.fejer@hotmail.de>
2023-12-08 15:01:53 +0100

    day8 - part1

---
+++ day8.py
@@ -0,0 +1,20 @@
+import math
+
+ll = [x for x in open("day8.txt").read().strip().split('\n\n')]
+instructions = list(ll[0])
+conn = {}
+
+for l in ll[1].split("\n"):
+	a = l.split(" ")[0]
+	b = l.split("(")[1].split(",")[0]
+	c = l.split(" ")[3].split(")")[0]
+	conn[a] = (b, c)
+pos = 'AAA'
+idx = 0
+
+while pos != 'ZZZ':
+	d = instructions[idx%len(instructions)]
+	pos = conn[pos][0 if d == 'L' else 1]
+	idx += 1
+
+print("p1", idx)

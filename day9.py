779079fbb80f9d2465a46e78b5b77ea82cc9f6d5
Ryukyo <florian.fejer@hotmail.de>
2023-12-09 21:16:38 +0100

    day9 - part2

--- day9.py
+++ day9.py
@@ -1,11 +1,16 @@
 history = [[int(i) for i in s.split()] for s in open('day9.txt').read().split('\n') if s.strip()]
 
 def extrapolate(history_entry):
+        # base case: all elements 0
         if sum(i != 0 for i in history_entry) == 0:
             return 0
+        
         m = []
         for i in range(len(history_entry) - 1):
+            # calculate the difference between 2 consecutive elements
             m.append(history_entry[i+1] - history_entry[i])
+        # return the most recent data history entry and recursively extrapolate on the calculated differences
         return history_entry[-1] + extrapolate(m)
 
 print(sum(extrapolate(i) for i in history))
+print(sum(extrapolate(i[::-1]) for i in history))
